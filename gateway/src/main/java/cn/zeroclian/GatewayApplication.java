2f1e7f86a72c7af7be670dedecf5d0da9238db88
ZeroClian <zeroycl@163.com>
2022-11-07 21:50:54 +0800

    解决跨域问题

--- gateway/src/main/java/cn/zeroclian/GatewayApplication.java
+++ gateway/src/main/java/cn/zeroclian/GatewayApplication.java
@@ -5,7 +5,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
 
 /**
  * @author Justin
@@ -21,5 +26,24 @@ public class GatewayApplication {
         Environment env = app.run(args).getEnvironment();
         LOG.info("启动成功!!!");
         LOG.info("Gateway地址: http://127.0.0.1:{}", env.getProperty("server.port"));
+    }
+
+    /**
+     * 解决跨域问题
+     */
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(Boolean.TRUE);
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        config.addAllowedOriginPattern("*");
+        config.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
     }
 }
